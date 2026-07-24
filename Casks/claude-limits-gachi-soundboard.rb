918926367e5bab1dc81cc7431f560787b34dcab4
marblecake88 <ikoteng@gmail.com>
2026-07-24 22:41:29 +0300

    каск 1.3.1

--- Casks/claude-limits-gachi-soundboard.rb
+++ Casks/claude-limits-gachi-soundboard.rb
@@ -1,6 +1,6 @@
 cask "claude-limits-gachi-soundboard" do
-  version "1.3"
-  sha256 "03d58dd8ff1110fe0903f42e650d2acca04bb5eccaa6b653ba51a62b36277eba"
+  version "1.3.1"
+  sha256 "b51281584411fc4fe5ff71d2815306bd59be4b0c4985d864d86c39d0a1203c70"
 
   url "https://github.com/marblecake88/claude-limits-gachi-soundboard/releases/download/v#{version}/LimitNotifier.zip"
   name "Claude Limits Gachi Soundboard"
